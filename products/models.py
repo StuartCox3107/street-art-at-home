88950082503a9513097f8605c025128be45125ae
StuartCox3107 <coxstuart@hotmail.com>
2020-12-05 14:09:57 +0000

    products app added and first pruduct added to django

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    city = models.CharField(max_length=254)
+    description = models.TextField()
+    price_s = models.DecimalField(max_digits=6, decimal_places=2)
+    price_m = models.DecimalField(max_digits=6, decimal_places=2)
+    price_l = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
