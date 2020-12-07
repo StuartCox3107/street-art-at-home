54e0daf273b242d72d5141cbad78b01de2c90247
StuartCox3107 <coxstuart@hotmail.com>
2020-12-07 20:27:05 +0000

    updated styling for shopping bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,7 +11,7 @@ def bag_contents(request):
 
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
+        total += quantity * product.price_l
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
