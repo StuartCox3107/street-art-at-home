0fe372342383b344fa013204fc9ae375b94e4d4e
StuartCox3107 <coxstuart@hotmail.com>
2020-12-13 12:21:15 +0000

    order number generator added to models in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -22,6 +22,12 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
