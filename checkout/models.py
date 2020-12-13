df74bc0ecb8c74889fe87fee7bdf73dd663cc074
StuartCox3107 <coxstuart@hotmail.com>
2020-12-13 12:32:38 +0000

    checkout app and models migrated

--- checkout/models.py
+++ checkout/models.py
@@ -24,21 +24,43 @@ class Order(models.Model):
 
     def _generate_order_number(self):
         """
-        Generate a random, unique order number using UUID
+        generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
 
+    def update_total(self):
+        """
+        update grand total each time a line item is added
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.total = self.total
+        self.save()
+
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the order number
+        override the original save method to set the order number
         if it hasn't been set already.
         """
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
 
+    def __str__(self):
+        return self.order_number
+
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
