3cd3781ca86511db049adc1162d0b29a0094e3a8
StuartCox3107 <coxstuart@hotmail.com>
2020-12-14 19:56:05 +0000

    total field removed from django admin panel as not needed

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,4 +1,3 @@
-  
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
@@ -11,16 +10,14 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'date',
-                       'total',)
+    readonly_fields = ('order_number', 'date',)
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county', 'total',)
+              'street_address2', 'county',)
 
-    list_display = ('order_number', 'date', 'full_name',
-                    'total',)
+    list_display = ('order_number', 'date', 'full_name',)
 
     ordering = ('-date',)
 
--- checkout/views.py
+++ checkout/views.py
@@ -9,7 +9,6 @@ from bag.contexts import bag_contents
 
 import stripe
 
-
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
@@ -61,7 +60,7 @@ def checkout(request):
         if not bag:
             messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
-
+        
         current_bag = bag_contents(request)
         total = current_bag['total']
         stripe_total = round(total * 100)
@@ -70,7 +69,6 @@ def checkout(request):
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
-
         order_form = OrderForm()
 
     if not stripe_public_key:
@@ -83,7 +81,6 @@ def checkout(request):
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
     }
-
     return render(request, template, context)
 
 def checkout_success(request, order_number):
