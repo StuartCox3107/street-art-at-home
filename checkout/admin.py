d678d6cb852697e2933b07637f35c11d8ebff850
StuartCox3107 <coxstuart@hotmail.com>
2020-12-15 21:41:59 +0000

    total order amount field added to admin page

--- checkout/admin.py
+++ checkout/admin.py
@@ -10,14 +10,14 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'date',)
+    readonly_fields = ('order_number', 'date', 'total',)
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county',)
+              'street_address2', 'county', 'total',)
 
-    list_display = ('order_number', 'date', 'full_name',)
+    list_display = ('order_number', 'date', 'full_name', 'total',)
 
     ordering = ('-date',)
 
