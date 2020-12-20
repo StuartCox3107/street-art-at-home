64edb7fb04537ef0eb5c24b683a8fbab19de800e
StuartCox3107 <coxstuart@hotmail.com>
2020-12-20 12:14:23 +0000

    removed secret key and set debug

--- street_art_at_home/settings.py
+++ street_art_at_home/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'brl$!_#8r+@2*yi!u!u#^mozq=u3m8$7go%bua=vco=gm031ds'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['street-art-at-home.herokuapp.com', 'localhost']
 
