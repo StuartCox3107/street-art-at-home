7a65ccc8b449e68a757975e789736546d265abc2
StuartCox3107 <coxstuart@hotmail.com>
2020-12-21 21:52:41 +0000

    added HEAD to AWS allowed method

--- street_art_at_home/settings.py
+++ street_art_at_home/settings.py
@@ -14,14 +14,14 @@ from pathlib import Path
 import os
 import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# sECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
