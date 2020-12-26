f9f275d4adb96fa57fe0d689a5aad41b2de5e4a2
StuartCox3107 <coxstuart@hotmail.com>
2020-12-26 11:20:40 +0000

    blog setup migrated

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Blogentry(models.Model):
+    blogid = models.DecimalField(max_digits=6, decimal_places=0)
+    blogtitle = models.CharField(max_length=254)
+    blogtext = models.TextField()
+    blogimage_url = models.URLField(max_length=1024, null=True, blank=True)
+    blogimage = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
