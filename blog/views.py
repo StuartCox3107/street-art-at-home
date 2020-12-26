8b869de689b4ea24286ba5b11eab6cf14a2b2d6f
StuartCox3107 <coxstuart@hotmail.com>
2020-12-26 12:15:59 +0000

    blog views created

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Blogentry
 
 # Create your views here.
+
+def blog(request):
+    """a view to show all blog entries"""
+
+    blogentries = Blogentry.objects.all()
+
+    context = {
+        'blogentries': blogentries,
+    }
+
+    return render(request, 'blog/blog.html', context)
