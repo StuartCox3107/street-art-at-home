a9cd68551174f70ad53c70b25a4d0f24723c07e4
StuartCox3107 <coxstuart@hotmail.com>
2021-01-01 16:08:21 +0000

    heroku deploy

--- street_art_at_home/settings.py
+++ street_art_at_home/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['street-art-at-home.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,17 +120,17 @@ WSGI_APPLICATION = 'street_art_at_home.wsgi.application'
 # database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#if 'DATABASE_URL' in os.environ:
-#    DATABASES = {
-#        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#    }
-#else:
-DATABASES = {
-    'default': {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 # password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
